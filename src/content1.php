164843b79c7020313787b4c97bf3e52fa4d70e2e
martamae <wegnerma@onid.oregonstate.edu>
2015-05-03 05:43:10 -0500

    Added links to login.php on content.php

--- src/content1.php
+++ src/content1.php
@@ -2,9 +2,14 @@
     $username = $_POST['username'];
 
     if ($username == NULL || $username == '') {
-        echo 'A username must be entered. Click here to return to the login screen.';
+        echo 'A username must be entered. Click
+        <a href="http://web.engr.oregonstate.edu/~wegnerma/CS290/Assignment4/login.php">here</a>
+         to return to the login screen.';
     }
     else {
-            echo 'Hello ' . $username . ' you have visited this page ' . $n . ' time before';
+            echo 'Hello ' . $username . ' you have visited this page ' . $n . ' times before <br>';
+            echo 'Click
+            <a href="http://web.engr.oregonstate.edu/~wegnerma/CS290/Assignment4/login.php">here</a>
+             to logout <br>';
     }
 ?>
