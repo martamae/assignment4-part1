d1982313dbc3114ba00bd074f4af430120501706
martamae <wegnerma@onid.oregonstate.edu>
2015-05-03 06:30:36 -0500

    Session count working

--- src/content1.php
+++ src/content1.php
@@ -1,16 +1,37 @@
 <?php
-    $username = $_POST['username'];
+    session_start();
 
-    if ($username == NULL || $username == '') {
-        echo 'A username must be entered. Click
-        <a href="http://web.engr.oregonstate.edu/~wegnerma/CS290/Assignment4/login.php">here</a>
-         to return to the login screen.';
+    //ends session
+    if (isset($_POST['action']) && $_POST['action'] == 'end') {
+        $_SESSION = emptyArray();
+        session_destroy();
+        header("Location: http://web.engr.oregonstate.edu/~wegnerma/CS290/Assignment4/login.php", true);
     }
-    else {
-            echo 'Hello ' . $username . ' you have visited this page ' . $n . ' times before <br>';
+
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        //first time visiting site n = 0
+        if (!isset($_SESSION['n'])) {
+            $_SESSION['n'] = 0;
+        } //Increment n for every visit
+        else {
+            $_SESSION['n']++;
+        }
+
+        if(!isset($_SESSION['username'])) {
+            $_SESSION['username'] = $_POST['username'];
+        }
+
+        if ($_SESSION['username'] == NULL || $_SESSION['username'] == '') {
+            echo 'A username must be entered. Click
+                <a href="http://web.engr.oregonstate.edu/~wegnerma/CS290/Assignment4/login.php">here</a>
+                to return to the login screen.';
+        }
+        else {
+            echo 'Hello ' . $_SESSION[username] . ' you have visited this page ' . $_SESSION[n] . ' times before <br>';
             echo 'Click
-            <a href="http://web.engr.oregonstate.edu/~wegnerma/CS290/Assignment4/login.php">here</a>
-             to logout <br>';
+                <a href="http://web.engr.oregonstate.edu/~wegnerma/CS290/Assignment4/login.php">here</a>
+                 to logout <br>';
             echo '<a href="http://web.engr.oregonstate.edu/~wegnerma/CS290/Assignment4/content2.php">Content 2</a> <br>';
+        }
     }
 ?>
