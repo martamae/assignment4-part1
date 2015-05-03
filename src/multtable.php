8cd8cb60b4c7fa08262ec70d0fbe38cbaa86dbc4
martamae <wegnerma@onid.oregonstate.edu>
2015-05-03 03:27:33 -0500

    All error messages added and tested

--- src/multtable.php
+++ src/multtable.php
@@ -7,24 +7,87 @@
 	</head>
 	<body>';
 
+    //Get variable from URL
     $minMultiplicand = $_GET["min-multiplicand"];
     $maxMultiplicand = $_GET["max-multiplicand"];
     $minMultiplier = $_GET["min-multiplier"];
     $maxMultiplier = $_GET["max-multiplier"];
 
-    echo '<table>';
-    echo '<tr> <td>';
-    for ($j = $minMultiplier; $j <= $maxMultiplier; $j++) {
-         echo '<td>';
-         echo $j;
+    //Check variables
+    $printTable = True;
+
+    if ($minMultiplier == NULL) {
+        echo 'Missing parameter min-multiplier <br>';
+        $printTable = False;
     }
-    for ($i = $minMultiplicand; $i <= $maxMultiplicand; $i++){
+
+    if ($minMultiplicand == NULL) {
+        echo 'Missing parameter min-multiplicand <br>';
+        $printTable = False;
+    }
+
+    if ($maxMultiplier == NULL) {
+        echo 'Missing parameter max-multiplier <br>';
+        $printTable = False;
+    }
+
+    if ($maxMultiplicand == NULL) {
+        echo 'Missing parameter max-multiplicand <br>';
+        $printTable = False;
+    }
+
+    if ($minMultiplicand != NULL && $maxMultiplicand != NULL && $minMultiplicand > $maxMultiplicand) {
+        echo 'Minimum multiplicand larger than maximum <br>';
+        $printTable = False;
+    }
+
+    if ($minMultiplier != NULL && $maxMultiplier != NULL && $minMultiplier > $maxMultiplier) {
+        echo 'Minimum multiplier larger than maximum <br>';
+        $printTable = False;
+    }
+
+    if (!ctype_digit($minMultiplier) || $minMultiplier < 0) {
+        echo 'Minimum multiplier must be a valid integer <br>';
+        $printTable = False;
+    }
+
+    if (!ctype_digit($maxMultiplier) || $maxMultiplier < 0) {
+        echo 'Maximum multiplier must be a valid integer <br>';
+        $printTable = False;
+    }
+
+    if (!ctype_digit($maxMultiplicand) || $maxMultiplicand < 0) {
+         echo 'Maximum multiplicand must be a valid integer <br>';
+        $printTable = False;
+    }
+
+    if (!ctype_digit($minMultiplicand) || $minMultiplicand < 0) {
+        echo 'Minimum multiplicand must be a valid integer <br>';
+        $printTable = False;
+    }
+
+    if ($printTable) {
+        //Create table
+        echo '<table>';
+
         echo '<tr> <td>';
-        echo $i;
         for ($j = $minMultiplier; $j <= $maxMultiplier; $j++) {
             echo '<td>';
-            echo $j * $i;
+            echo $j;
         }
+
+        for ($i = $minMultiplicand; $i <= $maxMultiplicand; $i++) {
+            echo '<tr> <td>';
+            echo $i;
+
+            for ($j = $minMultiplier; $j <= $maxMultiplier; $j++) {
+                echo '<td>';
+                echo $j * $i;
+            }
+        }
+
+        echo '</table>';
     }
-    echo '</table>';
+
+    echo '</body>';
 ?>
