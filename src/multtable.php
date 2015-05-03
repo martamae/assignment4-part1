81afbc1b3ee850b5c1439f8df242df39a1bbe0ef
martamae <wegnerma@onid.oregonstate.edu>
2015-05-03 01:54:47 -0500

    GETs variables from the URL

---
+++ src/multtable.php
@@ -0,0 +1,17 @@
+
+<?php
+	echo '<!DOCTYPE html>
+	<head>
+	<meta charset="utf-8" />
+	<title>Assignment 4</title>
+	</head>
+	<body>';
+
+    $minMultiplicand = $_GET["min-multiplicand"];
+    $maxMultiplicand = $_GET["max-multiplicand"];
+    $minMultiplier = $_GET["min-multiplier"];
+    $maxMultiplier = $_GET["max-multiplier"];
+
+
+
+?>
