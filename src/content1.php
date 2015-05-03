7ae077150d1499217a0b98906fbc6587f02205c1
martamae <wegnerma@onid.oregonstate.edu>
2015-05-03 05:35:12 -0500

    Basic output on content.php

--- src/content1.php
+++ src/content1.php
@@ -1,7 +1,10 @@
 <?php
     $username = $_POST['username'];
 
-    //if ($username == NULL) {
-      //  echo 'A username must be entered. Click here to return to the login screen.';
-    //}
+    if ($username == NULL || $username == '') {
+        echo 'A username must be entered. Click here to return to the login screen.';
+    }
+    else {
+            echo 'Hello ' . $username . ' you have visited this page ' . $n . ' time before';
+    }
 ?>
