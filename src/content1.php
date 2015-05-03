acf36038839179016477be4ef49ab17a76c413de
martamae <wegnerma@onid.oregonstate.edu>
2015-05-03 05:29:33 -0500

    Username input from login.php POST to content1.php

---
+++ src/content1.php
@@ -0,0 +1,7 @@
+<?php
+    $username = $_POST['username'];
+
+    //if ($username == NULL) {
+      //  echo 'A username must be entered. Click here to return to the login screen.';
+    //}
+?>
---
+++ src/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Assignment 4</title>
+    </head>
+    <body>
+        <form action="http://web.engr.oregonstate.edu/~wegnerma/CS290/Assignment4/content1.php" method="post">
+            <input type="text" name ="username">
+            <input type="submit" value="Login"><br>
+        </form>
+<?php
+
+?>
+    </body>
+</html>
